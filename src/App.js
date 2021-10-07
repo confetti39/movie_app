7d47926c1884f2500e6c73ed7604364aa7d33dfb
“깃 <hy04730@naver.com>
2021-10-07 13:42:15 +0900

    map함수 사용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
+const foodILike = [
+  {
+    name: "Kimchi",
+    image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+  },
+  {
+    name: "bulgogi",
+    image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
+  },
+  {
+    name: "kimbap",
+    image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
+  },
+  {
+    name: "samgyetang",
+    image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
+  }
+];
+
+function Food({ name, picture }) {
+  return <div>
+    <h1>I like {name}</h1>
+    <img src={picture} />
+  </div>
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {foodILike.map(dish => (
+        <Food name={dish.name} pciture={dish.image} />
+      ))}
     </div>
   );
 }
-
 export default App;
