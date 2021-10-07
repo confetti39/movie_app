dd905a8e0ee4e6e67db274f68d9bae6c977f10cf
“깃 <hy04730@naver.com>
2021-10-07 20:22:17 +0900

    #4.0 Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: []
   }
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000)
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
   }
+  componentDidMount() {
+    this.getMovies();
+  }
+
   render() {
     const { isLoading } = this.state;
     return <div>
-      {isLoading ? "Loading" : "We are ready"}
+      {isLoading ? "Loading..." : "We are ready"}
     </div>
   }
 }
