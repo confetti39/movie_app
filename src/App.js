406b1bec3d92c942b5b71726cee36a313fa85fc7
“깃 <hy04730@naver.com>
2021-10-07 13:57:21 +0900

    #2.3 map Recap (06:51)

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import './App.css';
 
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
   },
   {
+    id: 2,
     name: "bulgogi",
     image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
   },
   {
+    id: 3,
     name: "kimbap",
     image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
   },
   {
+    id: 4,
     name: "samgyetang",
     image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
   }
@@ -22,14 +26,14 @@ const foodILike = [
 function Food({ name, picture }) {
   return <div>
     <h1>I like {name}</h1>
-    <img src={picture} />
+    <img src={picture} alt={name} />
   </div>
 }
 function App() {
   return (
-    <div className="App">
+    <div>
       {foodILike.map(dish => (
-        <Food name={dish.name} pciture={dish.image} />
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
       ))}
     </div>
   );
